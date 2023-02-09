bcf0fd8f0f60ec8adee152a09af9f488e2356316
PetrenkoNastacia <114257531+PetrenkoNastacia@users.noreply.github.com>
2023-02-09 21:59:31 +0200

    Update MyArrayList.java
    deleting a draft thing

--- HW9mod/ArrayList/MyArrayList.java
+++ HW9mod/ArrayList/MyArrayList.java
@@ -57,12 +57,6 @@ public class MyArrayList <T>  {
         return (T) arrList[index];
     }
 
-    @Override
-    public String toString() {
-        if (arrList == null) {
-            return " ";
-        }
-        return Arrays.toString(arrList);
-    }
+  
 
 }
