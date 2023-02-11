acea67b998db0d02885e4ff04b6d729c7060092c
Anastasiia Petrenko <petrenko.nastacia@gmail.com>
2023-02-11 21:08:49 +0200

    Update of MyHashMap

--- HW9mod/HashMap/MyHashMap.java
+++ HW9mod/HashMap/MyHashMap.java
@@ -1,46 +1,97 @@
 package HW9mod.HashMap;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Random;
+public class MyHashMap<K, V> {
+    private Node<K, V>[] buckets;
+    private int size = 0;
+    private int capacity = 16;
 
-public class MyHashMap<K,V> {
-
-    Object [] arr;
-
-    public MyHashMap(){
-        arr = new Object [100000];
+    public MyHashMap() {
+        buckets = new Node[capacity];
     }
 
     public void put(K key, V value) {
-        int hash = key.hashCode();
-        int index = (int) (100000.0 * new Random(hash).nextDouble());
-        System.out.println("key = " + key);
-        System.out.println("hash = " + hash);
-        System.out.println("index = " + index);
-        arr[index] = value;
+        Node<K, V> newNode = new Node<>(key, value, null);
+        int newNodeIndex = hash(key) % capacity;
+        if (buckets[newNodeIndex] == null) {
+            buckets[newNodeIndex] = newNode;
+            size++;
+        } else {
+            Node<K, V> existingBucket = buckets[newNodeIndex];
+            if (existingBucket.key.equals(key)) {
+                while (existingBucket.next != null) {
+                    existingBucket = existingBucket.next;
+                }
+                existingBucket.next = newNode;
+                size++;
+            } else {
+                resize();
+                put(key, value);
+            }
+        }
+    }
+
+    public Node<K, V> get(K key) {
+        return buckets[hash(key) % capacity];
     }
 
     public void remove(K key) {
-        int hash = key.hashCode();
-        int index = (int) (100000.0 * new Random(hash).nextDouble());
-        arr[index] = null;
+        int hash = hash(key) % capacity;
+        buckets[hash] = null;
+        size--;
     }
 
     public void clear() {
-        Arrays.fill(arr,null);
+        buckets = new Node[capacity];
+        size = 0;
     }
 
     public int size() {
-        int count = 0;
-        count += Arrays.stream(arr).filter(Objects::nonNull).count();
-        return count;
+        return size;
     }
 
-    public V get(K key){
-        int hash = key.hashCode();
-        int index = (int) (100000.0 * new Random(hash).nextDouble());
-        return (V) arr[index];
+    private int hash(K key) {
+        return Math.abs(key.hashCode());
+    }
+
+    private void resize() {
+        while (true) {
+            capacity *= 3;
+            Node[] newBuckets = new Node[capacity];
+
+            boolean collision = false;
+            for (int i = 0; i < buckets.length; i++)
+                if (buckets[i] != null) {
+                    int newHash = hash(buckets[i].key) % capacity;
+                    if (newBuckets[newHash] != null) {
+                        collision = true;
+                        break;
+                    }
+                    newBuckets[newHash] = buckets[i];
+                }
+            if (!collision) {
+                buckets = newBuckets;
+                break;
+            }
+        }
+    }
+
+    public static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return key + ", " + value;
+        }
+
+
     }
 
 }
